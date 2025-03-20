23e947c49391b9d5dac10d64ace0aecf6588c45a
Tina Wulff <tina@tinawulff.dk>
2025-03-20 13:06:22 +0100

    webpack watch and more

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,35 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
+    mode: 'development',
+    entry: {
+        index: './src/index.js',
+        print: './src/print.js',
+      },
+      devtool: 'inline-source-map',
+      devServer: {
+        static: './dist',
+      },
+      plugins: [
+        new HtmlWebpackPlugin({
+         title: 'Development',
+        }),
+    ],
+    output: {
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   module: {
-      rules: [
-        {
-          test: /\.css$/i,
-          use: ['style-loader', 'css-loader'],
-        },
-        {
-            test: /\.(png|svg|jpg|jpeg|gif)$/i,
-            type: 'asset/resource',
-          },
-          {
-            test: /\.(woff|woff2|eot|ttf|otf)$/i,
-            type: 'asset/resource',
-          },
-          {
-            test: /\.(csv|tsv)$/i,
-            use: ['csv-loader'],
-          },
-          {
-            test: /\.xml$/i,
-            use: ['xml-loader'],
-          },
-      ],
-    },
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
 };
